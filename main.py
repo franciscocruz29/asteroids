4be1929ea62cbafa7546740699ef2221207ad844
Francisco Cruz <franciscohuertas25@gmail.com>
2025-02-17 15:18:06 -0500

    Limit the framerate to 60 FPS

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    clock = pygame.time.Clock()
+    dt = 0
+    
     run = True
 
     while run:
@@ -15,7 +17,9 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
-
+        
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
