80070058de1f979bf0ce0b527ba1ca8ccb39580e
Francisco Cruz <franciscohuertas25@gmail.com>
2025-02-18 10:21:11 -0500

    Draw a player

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import pygame
-from constants import * 
- 
+from constants import *
+from player import Player
+
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
-    
+
     run = True
 
     while run:
@@ -16,11 +18,12 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-        
+
         # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
 
+
 if __name__ == "__main__":
     main()
-
